4f75836b3f903f990eb9d4be1ad2e2b69d528dc2
Yerassyl <tleugazy98@gmail.com>
2021-07-04 10:25:35 +0600

    sdsd

--- lesson3/1.go
+++ lesson3/1.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a int
+	fmt.Scan(&a)
+	c := a % 2
+	fmt.Println(c)
+	if c == 0 {
+		fmt.Println("Четное")
+	} else {
+		fmt.Println("Не четное")
+	}
 }

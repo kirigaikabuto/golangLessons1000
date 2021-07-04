cc673886018d2bf001670e8ad21b7132b6b216ba
Yerassyl <tleugazy98@gmail.com>
2021-07-04 10:49:06 +0600

    sdsd

--- lesson3/2.go
+++ lesson3/2.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+	for counter := 0; counter < 5; counter++ {
+		fmt.Println(counter, "hello")
+	}
+
+	//1.counter = 0 -> Message counter+=1 counter<5 ->2
+	//2.counter = 1 -> Message counter+=1 counter<5 ->3
+	//3.counter = 2 -> Messsage
+
+}

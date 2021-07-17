cbed0f29695be80a11edad0ddedd993147c2e55e
Yerassyl <tleugazy98@gmail.com>
2021-07-17 11:36:29 +0600

    sdsd

--- lesson6/6.go
+++ lesson6/6.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateArray(e, l int) []int {
+	arr := []int{}
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i <= l; i++ {
+		v := rand.Intn(e)
+		arr = append(arr, v)
+	}
+	return arr
+}
+
 func main() {
+	k := generateArray(10, 5)
+	fmt.Println(k)
 
 }

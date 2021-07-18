27688ba8cbc9f48035f1397766ec5f49a3479547
Yerassyl <tleugazy98@gmail.com>
2021-07-18 10:15:05 +0600

    sdsd

--- lesson7/2.go
+++ lesson7/2.go
@@ -8,15 +8,11 @@ func main() {
 	arrAll := [][]int{
 		arr1,
 		arr2,
-		{4,5,6,12,23,41,3,},
+		{4, 5, 6, 12, 23, 41, 3},
 	}
-	fmt.Println(arrAll[1][4])
-	//rows, columns
-	//  0 1 2 3 4 5
-	//0 1 2 3 4 5
-	//1 4 5 6 7 8
-	//2
-	//3
-	//4
-	//5
+	for i := 0; i < len(arrAll); i++ {
+		for j := 0; j < len(arrAll[i]); j++ {
+			fmt.Println(arrAll[i][j])
+		}
+	}
 }

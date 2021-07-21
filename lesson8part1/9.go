139c5df7bd07c2509b0ecf2c037564debf5924d4
Yerassyl <tleugazy98@gmail.com>
2021-07-21 20:09:16 +0600

    sdsd

---
+++ lesson8part1/9.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ifSymbolNotExist(word, symbol string) bool {
+	isNotExist := true
+	for j := 0; j < len(word); j++ {
+		if string(word[j]) == symbol {
+			isNotExist = false
+			break
+		}
+	}
+	return isNotExist
+}
+
+func makeFilter(s string) string {
+	result := ""
+	words := strings.Split(s, " ")
+	filtered := []string{}
+	for i := 0; i < len(words); i++ {
+		element := words[i]
+		if ifSymbolNotExist(element, "e") {
+			filtered = append(filtered, element)
+		}
+	}
+	result = strings.Join(filtered, " ")
+	return result
+}
+
+func main() {
+	s := "asdsd dfdfd qwetx xcxsdsew"
+	fmt.Println(makeFilter(s))
+}

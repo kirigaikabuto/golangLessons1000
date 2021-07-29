b04282edd749ac88f8dd0101370350e02d3ace50
Yerassyl <tleugazy98@gmail.com>
2021-07-29 19:27:33 +0600

    sdsd

--- lesson11/5.go
+++ lesson11/5.go
@@ -6,12 +6,16 @@ type User struct {
 	Username string
 }
 
+func changeUsername(u User) {
+	u.Username = "111111"
+}
+
 func main() {
-	user1 := &User{"1234"}
+	user1 := User{"1234"}
 	user2 := user1
 	fmt.Println(user1)
 	fmt.Println(user2)
-	user1.Username = "123"
+	changeUsername(user1)
 	fmt.Println(user1)
 	fmt.Println(user2)
 }

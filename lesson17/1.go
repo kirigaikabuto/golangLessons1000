2c2c96def4be7c1e94a5cdc6b4363600d95322f8
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-08-21 10:37:36 +0600

    sdsd

--- lesson17/1.go
+++ lesson17/1.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/kirigaikabuto/golanglessons1000/lesson17/models"
+	"log"
+)
 
+func main() {
+	userStore := models.NewUserStore("localhost", "5432", "kirito", "12345")
+	u := models.User{
+		Username: "user1",
+		Password: "user1",
+	}
+	newUser, err := userStore.Create(u)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(newUser)
 }

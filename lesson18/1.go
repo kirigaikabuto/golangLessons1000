a3225ce98eb505d3756d5fc775860739ed0e4213
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-08-22 10:21:56 +0600

    sdsd

--- lesson18/1.go
+++ lesson18/1.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/kirigaikabuto/golanglessons1000/lesson18/products"
+)
 
+func main() {
+	p1 := &products.Product{Name: "product1", Price: 300}
+	p1.Save()
+	fmt.Println(p1)
 }
---
+++ lesson18/products/model.go
@@ -0,0 +1,74 @@
+package products
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+var (
+	Database *sql.DB
+)
+
+type Product struct {
+	Id    string
+	Name  string
+	Price int
+}
+
+//procedure which start when only start package
+func init() {
+	//connection to database
+	createTableQuery := `
+		create table if not exists products(
+			id text,
+			name text,
+			price int,
+			primary key(id)
+		);
+	`
+	host := "localhost"
+	port := "5432"
+	user := "setdatauser" //postgres
+	database := "lesson18"
+	password := "123456789" //admin
+	params := "sslmode=disable"
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?%s", user, password, host, port, database, params)
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	Database = db
+}
+
+func (p *Product) Save() {
+	p.Id = uuid.New().String()
+	queryInsert := "insert into products (id, name, price) values ($1, $2, $3)"
+	result, err := Database.Exec(queryInsert, p.Id, p.Name, p.Price)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	n, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if n <= 0 {
+		log.Fatal(err)
+		return
+	}
+}
